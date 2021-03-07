4b29c10d532efde3e741e6ed2e81fc5d4767a890
Jorge <jorgediazok@gmail.com>
2021-03-07 15:50:46 -0300

    added hero and video

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowRight,
+  ArrowForward,
+} from './HeroElements';
+import Button from '../../components/ButtonElement';
+import Video from '../../videos/video.mp4';
+
+const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Adventures Everywhere</HeroH1>
+        <HeroP>
+          Sign Up to Join Our Community of travellers from all over the world.
+          We share experiences, tips, and wonderful stories with other
+          travelers.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="/signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
