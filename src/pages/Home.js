ee0f6b4f1bd7efdb69caa042a6d42734236baec5
Jorge <jorgediazok@gmail.com>
2021-03-08 00:11:01 -0300

    added info elements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Hero from '../components/Hero/Hero';
+import InfoSection from '../components/InfoSection/InfoSection';
 import Navbar from '../components/Navbar/Navbar';
 import Sidebar from '../components/Sidebar/Sidebar';
 
@@ -19,6 +20,7 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <Hero />
+      <InfoSection />
     </>
   );
 };
